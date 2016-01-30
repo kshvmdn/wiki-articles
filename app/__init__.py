e3697561f52fca0ce61f2eb70f4ae86a24177988
Kashav Madan <kshvmdn@gmail.com>
2016-01-30 11:06:16 -0500

    Remove unneeded requirements

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, json, jsonify
+from flask import Flask, render_template
 from app.wikipedia import get_search_articles, get_random_articles
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def render_random_content():
-    return render_template('posts.html', articles=get_random_articles(5))
+def show_random_posts():
+    return render_template('articles.html', articles=get_random_articles(5))
 
 
 @app.route('/search/<query>')
 @app.route('/category/<query>')
 def show_posts(query):
-    return render_template('posts.html', query=query, articles=get_search_articles(query))
+    return render_template('articles.html', query=query,
+                           articles=get_search_articles(query))
