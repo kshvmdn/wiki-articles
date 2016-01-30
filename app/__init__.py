eadc483a9fe3834327580e95d349562720a9a0b3
Kashav Madan <kshvmdn@gmail.com>
2016-01-30 10:27:47 -0500

    Update category route to show top posts for given category

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, json, jsonify
 
 app = Flask(__name__)
-from app.wikipedia import get_articles as load_content
+from app.wikipedia import *
 
 @app.route('/')
 def render_random_content():
-    content = load_content()
-    return render_template('posts.html', articles=content)
+    return render_template('posts.html', articles=get_random_articles(2))
 
 
 @app.route('/category/<category_name>')
 def show_category_posts(category_name):
-    return render_template('category.html', category=category_name)
+    return render_template('posts.html', query=category_name, articles=get_articles_for_search(category_name))
--- app/wikipedia/__init__.py
+++ app/wikipedia/__init__.py
@@ -1,9 +1,8 @@
 import wikipedia
+import random
 
 
-def get_articles(n=2):
-    articles = wikipedia.random(n)
-    print(articles)
+def parse_articles(articles):
     data = {}
     for article in articles:
         try:
@@ -19,3 +18,14 @@ def get_articles(n=2):
         }
         print(article)
     return data
+
+
+
+def get_articles_for_search(query):
+    articles = wikipedia.search(query)
+    random.shuffle(articles)
+    return parse_articles(articles[:7])
+
+
+def get_random_articles(n=5):
+    return parse_articles(wikipedia.random(n))
